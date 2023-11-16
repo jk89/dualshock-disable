753786f1408068232e52ea9b7ed964aa2cfcf0b4
Jonathan Kelsey <jonathan.s.kelsey@gmail.com>
2023-11-16 13:17:51 +0000

    fix module

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,2 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getDevices = void 0;
 const getDevices = () => [];
-exports.getDevices = getDevices;
+export { getDevices };
